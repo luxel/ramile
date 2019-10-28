28f3e93c331c133f859ba13db9ed435f90fdc778
Dectinc Chen <chen@shijiang.info>
2019-10-28 12:06:56 +0800

    Test: add python process tests

---
+++ tests/test_python.py
@@ -0,0 +1,19 @@
+import pytest
+from ramile import *
+
+
+def test_file__init__():
+    """ Test with __init__.py from fire project:
+    -------------------------------------------------------------------------------
+Language                     files          blank        comment           code
+-------------------------------------------------------------------------------
+Python                       1              4            14                6
+-------------------------------------------------------------------------------
+    """
+    project = Project('data/fire',
+                      3000, 'test-output.docs')
+    project.run()
+    assert project.info.lines_extracted == 6
+    assert project.info.lines_skipped_blank == 4
+    assert project.info.lines_skipped_comments == 14
+    return
